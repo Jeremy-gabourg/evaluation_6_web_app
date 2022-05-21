37073b955d048346aaed1111526c7ec45d7c2ffd
Jérémy GABOURG <gabourg.developpement@gmail.com>
2022-05-21 17:32:47 +0200

    Add bootstrap with composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '34c03ae19aba457f9f80c9672ea4d5726958c2e8',
+        'reference' => '02afe9e0fb26c80621e6eb5cbac0b912d1544bf1',
         'name' => '__root__',
         'dev' => true,
     ),
@@ -16,8 +16,23 @@
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '34c03ae19aba457f9f80c9672ea4d5726958c2e8',
+            'reference' => '02afe9e0fb26c80621e6eb5cbac0b912d1544bf1',
             'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.1.3',
+            'version' => '5.1.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'reference' => '1a6fdfae6be09b09eaced8f0e442ca6f7680a61e',
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.1.3',
+            ),
         ),
     ),
 );
