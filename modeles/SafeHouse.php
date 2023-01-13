9f144e8938edd36713bd4e6831107ea1d2f1e536
Jérémy GABOURG <gabourg.developpement@gmail.com>
2023-01-13 17:52:48 +0100

    Finalise display_safe_houses_list file

--- modeles/SafeHouse.php
+++ modeles/SafeHouse.php
@@ -5,8 +5,8 @@ class SafeHouse
     private int $id;
     private string $address;
     private string $type;
-    private bool $is_available;
-    private int $mission;
+    private int $is_available;
+    private int|null $mission;
     private int $country;
 
     /**
@@ -58,17 +58,17 @@ class SafeHouse
     }
 
     /**
-     * @return bool
+     * @return int
      */
-    public function isIsAvailable(): bool
+    public function getIsAvailable(): int
     {
         return $this->is_available;
     }
 
     /**
-     * @param bool $is_available
+     * @param int $is_available
      */
-    public function setIsAvailable(bool $is_available): void
+    public function setIsAvailable(int $is_available): void
     {
         $this->is_available = $is_available;
     }
@@ -104,6 +104,7 @@ class SafeHouse
     {
         $this->country = $country;
     }
+
 
     public function displaySafeHousesList (): void
     {
@@ -156,8 +157,7 @@ class SafeHouse
                                                 <td>'.$country->getFrenchName().'</td>
                                                 <td>'.$type->getName().'</td>';
 
-                            $mission = $safeHouse->getMission();
-                            if($mission=="" || $mission==null) {
+                            if($safeHouse->getIsAvailable()==0) {
                                 echo '    <td class="text-danger text-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                                               <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
