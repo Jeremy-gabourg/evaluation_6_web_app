e81d11f928da48c40b581f2968dc45f6de938f5d
Jérémy GABOURG <gabourg.developpement@gmail.com>
2023-01-15 22:38:59 +0100

    Add gitignore and files to deploy on Fly.io

--- controleurs/bdd_connexion.php
+++ controleurs/bdd_connexion.php
@@ -1,3 +1,8 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=international_spy_agency', 'root', '');
+$host = "localhost";
+$dbname = "international_spy_agency";
+$charset = "utf8";
+$port="3306";
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset;port=$port", 'root', '',);
--- vues/front_header.php
+++ vues/front_header.php
@@ -2,12 +2,12 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>AIE : Agence Internationale d'Espionnage</title>
+    <title>KGB : Comité pour la sécurité de l'état</title>
     <link rel="stylesheet"href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
     <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <meta name="viewport", content="width=device-width, initial-scale=1">
-    <meta name="description", content="Bienvenue sur le site internet d'espionnage le plus libre du monde. Entrez,
+    <meta name="description", content="Bienvenue sur le site internet du KGB. Entrez,
     récupérez votre mission et accomplissez la! Une fois la preuve du travail réalisé fournie, un virement vous sera fait
     dans les 48H">
 </head>
@@ -19,8 +19,8 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
                 <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
             </svg>
-            <span class="fw-bold d-none d-md-inline-flex">Agence Internationale d'Espionnage</span>
-            <span class="fw-bold d-inline-flex d-md-none">AIE</span>
+            <span class="fw-bold d-none d-md-inline-flex">Comité pour la sécurité de l'Etat</span>
+            <span class="fw-bold d-inline-flex d-md-none">KGB</span>
         </a>
         <span class="p-1 fw-bold align-self-end">
         <?php
