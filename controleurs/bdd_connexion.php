e183ce6a20a6c998e6ae9761c3b9a082bf74e672
Jérémy GABOURG <gabourg.developpement@gmail.com>
2023-05-06 16:45:36 +0200

    Modify bdd_connexion file to take account of the planethoster db

--- controleurs/bdd_connexion.php
+++ controleurs/bdd_connexion.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "mysql";
-$dbname = "kgb_db";
+$host = "localhost";
+$dbname = "jyuvhkkx_evaluation_kgb";
 $charset = "utf8";
 $port="3306";
 
-$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset;port=$port", username:'root', password:'super-secret-password',);
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset;port=$port", username:'jyuvhkkx_admin', password:'Monsupermotdepasse1',);
