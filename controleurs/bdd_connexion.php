87b0e543d677c516601a2ae2048f4e50db437d83
Jérémy GABOURG <gabourg.developpement@gmail.com>
2023-06-26 15:08:07 +0200

    Add the serveur connexion datas in the 'bdd-connexion' file

--- controleurs/bdd_connexion.php
+++ controleurs/bdd_connexion.php
@@ -1,8 +1,21 @@
 <?php
 
+// Dockerfile local
 $host = "mysql";
 $dbname = "kgb_db";
 $charset = "utf8";
 $port="3306";
+$username = 'root';
+$password = 'super-secret-password';
 
-$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset;port=$port", username:'root', password:'super-secret-password',);
+// Serveur Planet Hoster
+// $host = "localhost";
+// $dbname = "jyuvhkkx_evaluation_kgb";
+// $charset = "utf8";
+// $port ="3306";
+// $username ='jyuvhkkx_admin';
+// $password = 'Monsupermotdepasse1';
+
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset;port=$port", username:$username, password:$password,);
+
